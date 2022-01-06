dfaa8bfa9004bbca274fbec3839be89094b2d7d5
Mark Draughn <mark@dsoftco.com>
2022-01-06 15:31:52 -0600

    Add result caching

--- bee_word_list/__init__.py
+++ bee_word_list/__init__.py
@@ -1,17 +1,20 @@
 import logging
+from functools import lru_cache
+
 import azure.functions as func
 
 from SpellingBeePlus import SpellingBeePlus
 
+_context = None
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    global _context
     logging.info('Python HTTP trigger function processed a request.')
 
     pattern = req.route_params.get('pattern')
     if pattern:
-        bee = SpellingBeePlus()
-        words = [w+'\n' for w in bee.get_words(pattern)]
-        return func.HttpResponse(''.join(words))
+        content = build_word_list(pattern)
+        return func.HttpResponse(content, headers={})
     else:
         return func.HttpResponse(
             "Please pass a pattern as the next component of the path",
@@ -27,3 +30,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     #         "Please pass a name as the next component of the path",
     #         status_code=400
     #     )
+
+@lru_cache(maxsize=10)
+def build_word_list(pattern):
+    global _context
+    if _context is None:
+        _context = SpellingBeePlus()
+    words = [w + '\n' for w in _context.get_words(pattern)]
+    content = ''.join(words)
+    return content
